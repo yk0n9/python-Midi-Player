b052ef668ec94aab4146fc29be17395b8beb7f4e
Ykong <66518156+Ykong1337@users.noreply.github.com>
2022-11-03 11:30:27 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,25 +1,23 @@
 from __future__ import print_function
+from threading import Thread
+from pynput.keyboard import Listener, Key
+from tkinter import filedialog
 import ctypes
 import time
 import pydirectinput
 import mido
-from future.types.newbytes import unicode
-
 import util
 import tkinter
 import sys
 import os
-from threading import Thread
-from pynput.keyboard import Listener, Key
-from tkinter import filedialog
 
 pydirectinput.PAUSE = 0
 keys = util.mapping
 
 
 class Play(Thread):
-    Exit = False
     go = False
+    switch = False
     speed = 1.0
 
     def __init__(self):
@@ -27,26 +25,16 @@ class Play(Thread):
 
     def run(self):
 
-        while not self.Exit:
+        while True:
             root = tkinter.Tk()
             root.withdraw()
             filepath = filedialog.askopenfilename()
 
             try:
                 mid = mido.MidiFile(filepath)
-            except:
+            except Exception.__base__:
                 print("file error")
                 os._exit(0)
-
-            messages = []
-            node_type = ['note_on', 'note_off']
-            for i, track in enumerate(mid.tracks):
-                print(f'Track {i}')
-                for msg in track:
-                    info = msg.dict()
-                    if info['type'] in node_type:
-                        if info['type'] == 'note_on':
-                            messages.append(info)
 
             speed = str(input("Input play speed(x) (default: 1.0)"))
             if speed == "":
@@ -55,21 +43,25 @@ class Play(Thread):
                 self.speed = float(speed)
 
             shift = 0
-            while shift == 0:
-                auto_tune = input("Turn on automatic transposition ? (0/1) (default: 0)")
-                if auto_tune == "1":
-                    shift, score = util.get_shift_best_match(messages)
-                    print("Transposition: ", shift)
-                    print("Hit: ", f'{score:.2%}')
-                    break
-                elif auto_tune == "0" or auto_tune == "":
-                    shift = 0
-                    break
+            auto_tune = input("Turn on automatic transposition ? (0/1) (default: 0)")
+            if auto_tune == "1":
+                messages = []
+                node_type = ['note_on', 'note_off']
+                for i, track in enumerate(mid.tracks):
+                    print(f'Track {i}')
+                    for msg in track:
+                        info = msg.dict()
+                        if info['type'] in node_type:
+                            if info['type'] == 'note_on':
+                                messages.append(info)
+                shift, score = util.get_shift_best_match(messages)
+                print("Transposition: ", shift)
+                print("Hit: ", f'{score:.2%}')
+            elif auto_tune == "0" or auto_tune == "":
+                shift = 0
 
             print()
             print("Please press \"Space\" to start playback")
-            print()
-            print("Please press \"Tab\" to stop playback")
             print()
             print("Please press \"Shift\" to stop playback and reselect the file")
             print()
@@ -85,8 +77,12 @@ class Play(Thread):
             start_time = time.time()
             input_time = 0.0
             for msg in mid:
-                if not self.go:
+                if self.switch:
                     break
+
+                while True:
+                    if self.go:
+                        break
 
                 input_time += msg.time / self.speed
 
@@ -98,8 +94,10 @@ class Play(Thread):
 
                 if msg.type == 'note_on' and msg.note + shift in keys:
                     pydirectinput.press(keys[msg.note + shift])
+
             print("Play ends")
             self.go = False
+            self.switch = False
 
 
 def main():
@@ -110,7 +108,7 @@ def main():
         if key == Key.space:
             playing.go = True
         elif key == Key.shift:
-            playing.go = False
+            playing.switch = True
         elif key == Key.up:
             playing.speed += 0.1
             print("Playback speed is", f'{playing.speed:.1f}')
@@ -119,10 +117,6 @@ def main():
             playing.speed -= 0.1
             print("Playback speed is", f'{playing.speed:.1f}')
             print()
-        elif key == Key.tab:
-            playing.go = False
-            playing.Exit = True
-            sys.exit(0)
 
     with Listener(
             on_press=(),
@@ -135,7 +129,7 @@ if __name__ == '__main__':
     def is_admin():
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
+        except Exception.__base__:
             return False
 
 
@@ -143,5 +137,3 @@ if __name__ == '__main__':
         main()
     else:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-
-
